c63dd231df5f9e0afb935946f38690317ee83d51
Malgorzata Pachocki <37968756+lamalgopach@users.noreply.github.com>
2021-12-13 09:06:36 -1000

    Day 11 part 2

--- 11.py
+++ 11.py
@@ -58,21 +58,37 @@ def zero_octopi_above_nine(octopi):
 	return octopi
 # count flashes:
 def count_flashes(octopi):
-	flashes = 0
+	new_flashes = 0
+	all_flashing = False
 	for line in octopi:
 		for octopus in line:
 			if octopus == 0:
-				flashes += 1
-	return flashes
+				new_flashes += 1
+	if new_flashes == len(octopi) * len(octopi[0]):
+		all_flashing = True
+	return new_flashes, all_flashing
 
 steps = 1
 flashes = 0
+all_flashing = False
+flashes_after_100_steps = 0
 
-while steps <= 100:
+while not all_flashing:
+
+	new_flashes = 0
 	octopi, to_visit = increase_energy_by_one(octopi)
 	octopi = increase_the_energy_level_of_adjacent(octopi, to_visit)
 	octopi = zero_octopi_above_nine(octopi)
-	flashes += count_flashes(octopi)
+	new_flashes, all_flashing = count_flashes(octopi)
+	flashes += new_flashes
+	if steps == 100:
+		flashes_after_100_steps = flashes
 	steps += 1
+	print("after step", steps - 1)
+	for line in octopi:
+		print(line)
+	print()
+print(flashes_after_100_steps)
+print(steps - 1)
 
-print(flashes)
+
